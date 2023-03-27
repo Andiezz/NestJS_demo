ab0d1f068afb2adfd4e470cd02ae8ad1cb1984e8
Vũ Thành An <superlegend27@gmail.com>
2023-03-27 17:25:43 +0700

    bad code: dependency

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,20 +1,37 @@
-import { Controller, Get, Post, Body, Param  } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, NotFoundException  } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  // Service is creating its own dependencies
+  //! DON'T DO THIS 
+  //? USE DEPENDENCY INJECTION
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
   listMessages() {
-    
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessages(@Body() body: CreateMessageDto) { //? class transformer
-    console.log(body)
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessages(@Param('id') id: string) {
-    console.log(id)
+  async getMessages(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      //? find all exception Nest by reveal the exception in explorer view
+      throw new NotFoundException('Message not found');
+    }
+
+    return message;
   }
 }
