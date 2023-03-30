34debf7241c50ae8d5d3b5f1fe461a28d6767844
Andiezz <superlegend27@gmail.com>
2023-03-30 11:26:31 +0700

    finish adding CRUD

--- mycv/src/users/user.entity.ts
+++ mycv/src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity() //? tell TypeORM to create a new TABLE to model this class / collection
 export class User {
@@ -11,4 +11,20 @@ export class User {
   @Column()
   password: string;
   //? all done at startup by synchronize true (ì we add a new field)
+
+  //? HOOK only work when pass in the user entity
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
 }
--- mycv/src/users/users.service.ts
+++ mycv/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -9,14 +9,39 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
-    const user = this.repo.create({ email, password })
+    const user = this.repo.create({ email, password });
 
     return this.repo.save(user);
   }
 
   findOne(id: number) {
-    const user = this.repo.findOneBy({ id: id});
+    const user = this.repo.findOneBy({ id: id });
 
     return user;
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found')
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found')
+    }
+
+    return this.repo.recover(user);
   }
 }
